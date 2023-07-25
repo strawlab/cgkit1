b0a152a16941e1156c79105cf1cb09af400e9f4f
Andrew Straw <strawman@astraw.com>
2023-07-25 08:37:00 +0200

    remove unneeded cython directive

--- cgtypes.pyx
+++ cgtypes.pyx
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# cython: language_level=2, c_api_binop_methods=True
+# cython: c_api_binop_methods=True
 ######################################################################
 # cgtypes - vec3, vec4, mat3, mat4, quat
 #
